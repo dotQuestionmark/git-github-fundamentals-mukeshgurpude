1f80047c8d3ff825a463d73f5634ff48ec6fe7a9
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-04-02 18:17:52 +0530

    Excluded executables

---
+++ Day-02/variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+// Define namespace
+using namespace std;
+
+// Main function
+main(void){
+  int a = 5;    // Declare a variable with integer data type
+  cout<<"Value of a: "<< a;    //output above integer
+  return 0;
+}
